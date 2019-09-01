fab3776cc47b3e489ac339f4dc64bedcce40facd
George Savill <gpsavill@gmail.com>
2019-09-01 15:09:22 +0100

    Fix bug with the menu icon

--- src/App.js
+++ src/App.js
@@ -7,33 +7,16 @@ class SideMenu extends React.Component {
         this.sideMenuEntries = ["home", "shop", "about"];
     }
     render(){
-        return(<div className="sideMenu">{this.sideMenuEntries.map((entry) => <SideMenuEntry key={entry} value={entry} setPage={this.props.setPage}/>)}</div>)
+        return(<div className="sideMenu">{this.sideMenuEntries.map((entry) => <SideMenuEntry key={entry} value={entry} setPage={this.props.setPage} closeMenu={this.props.closeMenu}/>)}</div>)
     }
 }
 
 function SideMenuEntry(props){
-    function changePage() {props.setPage(props.value)};
+    function changePage() {
+        props.setPage(props.value);
+        props.closeMenu();
+    };
     return(<div className="sideMenuEntry" onClick={changePage}>{props.value}</div>)
-}
-
-class TopNav extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {menuOpen: false};
-        this.openCloseMenu = this.openCloseMenu.bind(this);
-    }
-    openCloseMenu(){
-        this.setState(state => ({menuOpen: !state.menuOpen}));
-        this.props.setPage(this.state.menuOpen ? this.props.previousPage : "menu");
-    }
-    render(){
-    return(
-        <div className="topNav">{this.props.currentPage}
-            <div onClick={this.openCloseMenu} className={this.state.menuOpen ? "menuBtn change" : "menuBtn"}>
-                <div className="bar1"></div><div className="bar2"></div><div className="bar3"></div>
-            </div>
-        </div>
-    )}
 }
 
 function HomePage(){
@@ -51,11 +34,21 @@ class App extends React.Component {
         super(props);
         this.state = {
             currentPage: "home",
-            previousPage: ""
+            previousPage: "",
+            menuOpen: false
         }
         this.setPage = this.setPage.bind(this);
+        this.openCloseMenu = this.openCloseMenu.bind(this);
+        this.closeMenu = this.closeMenu.bind(this);
     }
     setPage(page){this.setState(() => ({currentPage: page, previousPage: this.state.currentPage}))}
+    openCloseMenu(){
+        this.setState(state => ({menuOpen: !state.menuOpen}));
+        this.setPage(this.state.menuOpen ? this.state.previousPage : "menu");
+    }
+    closeMenu(){
+        this.setState(() => ({menuOpen: false}));
+    }
     renderPage(page){
         switch(page){
             case "home":
@@ -65,7 +58,7 @@ class App extends React.Component {
             case "about":
                 return <AboutPage></AboutPage>
             case "menu":
-                return <SideMenu setPage={this.setPage}></SideMenu>
+                return <SideMenu setPage={this.setPage} closeMenu={this.closeMenu}></SideMenu>
             default:
                 return <HomePage></HomePage>
         }
@@ -73,11 +66,23 @@ class App extends React.Component {
     render(){
         return(
         <div className="app">
-            <TopNav previousPage={this.state.previousPage} currentPage={this.state.currentPage} setPage={this.setPage}></TopNav>
+            <TopNav previousPage={this.state.previousPage} currentPage={this.state.currentPage} setPage={this.setPage} menuOpen={this.state.menuOpen} openCloseMenu={this.openCloseMenu}></TopNav>
             <div className="pageContent">{this.renderPage(this.state.currentPage)}</div>
         </div>
         )
     }
 }
-
+class TopNav extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+    return(
+        <div className="topNav">{this.props.currentPage}
+            <div onClick={this.props.openCloseMenu} className={this.props.menuOpen ? "menuBtn change" : "menuBtn"}>
+                <div className="bar1"></div><div className="bar2"></div><div className="bar3"></div>
+            </div>
+        </div>
+    )}
+}
 export default App;
